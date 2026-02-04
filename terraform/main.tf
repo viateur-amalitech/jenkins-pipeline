a6331733a832fe9fe68a9521b3c98df5b836cc57
Viateur-akimana <akimanaviateur94@gmail.com>
2026-02-04 11:15:27 +0200

    feat: add Elastic IP for static endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,12 @@ module "web_server" {
   key_name      = var.key_name
 }
 
-output "web_server_public_ip" {
-  value = module.web_server.public_ip
+resource "aws_eip" "web_server_eip" {
+  instance = module.web_server.instance_id
+  domain   = "vpc"
 }
+
+output "web_server_public_ip" {
+  value = aws_eip.web_server_eip.public_ip
+}
+
--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = aws_instance.web_server.public_ip
 }
+
+output "instance_id" {
+  value = aws_instance.web_server.id
+}
